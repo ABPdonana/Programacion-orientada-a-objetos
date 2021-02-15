ee72cdd8974bc022c285d0039d955c10b560b955
Adrián Bedoya Patino <adrian.bedoya@iesdonana.org>
2021-02-15 17:01:17 +0100

    Creado el archivo movimiento.py con la clase Movimiento

---
+++ movimiento.py
@@ -0,0 +1,8 @@
+class Movimiento:
+    def __init__(self, concepto, cantidad):
+        self.__concepto = concepto
+        self.__cantidad = cantidad
+    def concepto(self):
+        return self.__concepto
+    def cantidad(self):
+        return self.__cantidad
