ee11ebf74c277e78bd8637547e06aa72fa57d0d1
Adrián Bedoya Patino <adrian.bedoya@iesdonana.org>
2021-02-15 16:55:37 +0100

    Creado el archivo Cliente.py con la clase Cliente

---
+++ cliente.py
@@ -0,0 +1,12 @@
+class Cliente:
+    def __init__(self, dni, nombre, apellidos):
+        self.__dni = dni
+        self.__nombre = nombre
+        self.__apellidos = apellidos
+
+    def dni(self):
+        return self.__dni
+    def nombre(self):
+        return self.__nombre
+    def apellidos(self):
+        return self.__apellidos
