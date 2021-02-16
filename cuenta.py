32126e6292b88fbed9876f03310730c8d8cf71c8
Adrián Bedoya Patino <adrian.bedoya@iesdonana.org>
2021-02-16 13:31:58 +0100

    modificaciones en cuenta.py

--- cuenta.py
+++ cuenta.py
@@ -1,8 +1,13 @@
 from movimiento import Movimiento
+from cliente import Cliente
 
 class Cuenta:
-    def __init__(self, numero, titular, saldo):
-        self.__numero = numero
+
+    __numero_cuenta = 0
+
+    def __init__(self, titular, saldo):
+        Cuenta.__numero_cuenta += 1
+        self.__numero = Cuenta.__numero_cuenta
         self.__titular = titular
         self.__movimientos = []
         self.__saldo = saldo
@@ -16,3 +21,9 @@ class Cuenta:
 
     def __set_saldo(self, cantidad):
         self.__saldo += cantidad
+
+    def numero(self):
+        return self.__numero
+
+    def titular(self):
+        return f'{self.__titular.apellidos()} {self.__titular.nombre()}'
